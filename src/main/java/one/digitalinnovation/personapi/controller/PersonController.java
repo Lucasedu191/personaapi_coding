48df34d20a6801b78c9dec90ba62b97a38bcf4d0
Lucas Eduardo <lucasatletico@hotmail.com>
2021-09-04 23:02:12 -0300

    desenvolvendo listar pessoas por ID

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -2,20 +2,22 @@ package one.digitalinnovation.personapi.controller;
 
 import one.digitalinnovation.personapi.dto.MessageResponseDTO;
 import one.digitalinnovation.personapi.dto.request.PersonDTO;
+import one.digitalinnovation.personapi.exception.PersonNotFoundException;
 import one.digitalinnovation.personapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
-@RequestMapping
-public class PersoController {
+@RequestMapping("/api/v1/people")
+public class PersonController {
 
     private PersonService personService;
     @Autowired
-    public PersoController(PersonService personService) {
+    public PersonController(PersonService personService) {
         this.personService = personService;
     }
 
@@ -23,5 +25,14 @@ public class PersoController {
     @ResponseStatus(HttpStatus.CREATED)
     public MessageResponseDTO createPerson (@RequestBody @Valid PersonDTO personDTO){
         return  personService.createPerson(personDTO);
+    }
+    @GetMapping
+    public List<PersonDTO> listAll(){
+        return personService.listAll();
+    }
+
+    @GetMapping("/{id}")
+    public PersonDTO findById(@PathVariable Long id) throws PersonNotFoundException {
+        return personService.findById(id);
     }
 }
